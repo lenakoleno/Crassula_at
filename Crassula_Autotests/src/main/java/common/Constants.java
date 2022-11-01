d8934549efd3d076714baf577e853bd2ce63e581
sofiyaefremova <sofiya.yefremova@list.ru>
2022-11-01 17:49:47 +0300

    refactoring

--- Crassula_Autotests/src/main/java/common/Constants.java
+++ Crassula_Autotests/src/main/java/common/Constants.java
@@ -1,8 +1,11 @@
 package common;
 
-public class Constants {
+public final class Constants {
     public static final String AMOUNT = "3.57";
     public static final String ACCOUNT_NUMBER = "13413119267";
     public static final String DESCRIPTION = "payment in gbp to friend";
     public static final String TEMPLATE_NAME = "testTemplate";
+
+    private Constants() {
+    }
 }
